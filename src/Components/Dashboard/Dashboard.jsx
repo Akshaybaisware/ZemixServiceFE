ba6ac4b129b855c495a0df6bbb9944604d39e062
Kaustubh <kaustubhr2001@gmail.com>
2024-04-04 21:59:50 +0530

    updation in dahboard

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -122,6 +122,403 @@ function Dashboard() {
                 fontSize={{ base: "0.7rem", md: "1.2rem" }}
                 fontWeight="bold"
               >
+                Total Registeration
+              </Text>
+            </Card>
+          </Link>
+        </Flex>
+        <Flex
+          direction={{ md: "column" }}
+          gap="25px"
+          justifyContent="center"
+          marginTop="30px"
+          align="center"
+        >
+          {/* Chnage Link */}
+          <Link
+            to={{
+              pathname: "/f",
+              // state: { users },
+            }}
+          >
+            <Card
+              as="flex"
+              width="auto"
+              textAlign="center"
+              height={{ base: "100%", md: "150px" }}
+              minW="150px"
+              maxH="150px"
+              bg="white"
+              // borderLeft="4px solid #FF0000"
+              borderRadius="12px"
+              boxShadow="md" // Add a shadow for a card-like appearance
+              p="2" // Adjust padding as needed
+              display="flex"
+              // flexDirection={{ base: "column", md: "column", lg: "column" }}
+              direction={{ base: "column", md: "row", lg: "row" }}
+              justifyContent={{ base: "center", md: "space-around" }}
+              alignItems="center"
+              gap="20px"
+            >
+              {/* <div style={{ flex: 1 }}> */}
+              <img
+                src={
+                  // ""
+                  TotalCumstmer
+                }
+                alt="Total Customers"
+              />
+              <Text
+                fontSize={{ base: "0.7rem", md: "1.2rem" }}
+                fontWeight="bold"
+              >
+                Pending Registeration
+              </Text>
+            </Card>
+          </Link>
+        </Flex>
+      </Flex>
+
+      <Flex direction={{ base: "column", md: "row" }} gap="25px">
+        <Flex
+          direction={{ md: "column" }}
+          gap="25px"
+          justifyContent="center"
+          marginTop="30px"
+          align="center"
+        >
+          {/* Chnage Link */}
+          <Link
+            to={{
+              pathname: "/f",
+              // state: { users },
+            }}
+          >
+            <Card
+              as="flex"
+              width="auto"
+              textAlign="center"
+              height={{ base: "100%", md: "150px" }}
+              minW="150px"
+              maxH="150px"
+              bg="white"
+              // borderLeft="4px solid #FF0000"
+              borderRadius="12px"
+              boxShadow="md" // Add a shadow for a card-like appearance
+              p="2" // Adjust padding as needed
+              display="flex"
+              // flexDirection={{ base: "column", md: "column", lg: "column" }}
+              direction={{ base: "column", md: "row", lg: "row" }}
+              justifyContent={{ base: "center", md: "space-around" }}
+              alignItems="center"
+              gap="20px"
+            >
+              {/* <div style={{ flex: 1 }}> */}
+              <img
+                src={
+                  // ""
+                  TotalCumstmer
+                }
+                alt="Total Customers"
+              />
+              <Text
+                fontSize={{ base: "0.7rem", md: "1.2rem" }}
+                fontWeight="bold"
+              >
+                Cancel User
+              </Text>
+              {/* <Heading size="lg" color="black" textAlign="center">
+                  {EmployeeCount}
+                </Heading> */}
+              {/* {users && (
+                  <Heading size="lg" color="black" textAlign="center">
+                    {users.length}
+                    {console.log("users length", users.length)}
+                  </Heading> */}
+
+              {/* </div> */}
+            </Card>
+          </Link>
+        </Flex>
+        <Flex
+          direction={{ md: "column" }}
+          gap="25px"
+          justifyContent="center"
+          marginTop="30px"
+          align="center"
+        >
+          {/* Chnage Link */}
+          <Link
+            to={{
+              pathname: "/f",
+              // state: { users },
+            }}
+          >
+            <Card
+              as="flex"
+              width="auto"
+              textAlign="center"
+              height={{ base: "100%", md: "150px" }}
+              minW="150px"
+              maxH="150px"
+              bg="white"
+              // borderLeft="4px solid #FF0000"
+              borderRadius="12px"
+              boxShadow="md" // Add a shadow for a card-like appearance
+              p="2" // Adjust padding as needed
+              display="flex"
+              // flexDirection={{ base: "column", md: "column", lg: "column" }}
+              direction={{ base: "column", md: "row", lg: "row" }}
+              justifyContent={{ base: "center", md: "space-around" }}
+              alignItems="center"
+              gap="20px"
+            >
+              {/* <div style={{ flex: 1 }}> */}
+              <img
+                src={
+                  // ""
+                  TotalCumstmer
+                }
+                alt="Total Customers"
+              />
+              <Text
+                fontSize={{ base: "0.7rem", md: "1.2rem" }}
+                fontWeight="bold"
+              >
+                Active User
+              </Text>
+            </Card>
+          </Link>
+        </Flex>
+      </Flex>
+      <Flex direction={{ base: "column", md: "row" }} gap="25px">
+        <Flex
+          direction={{ md: "column" }}
+          gap="25px"
+          justifyContent="center"
+          marginTop="30px"
+          align="center"
+        >
+          {/* Chnage Link */}
+          <Link
+            to={{
+              pathname: "/f",
+              // state: { users },
+            }}
+          >
+            <Card
+              as="flex"
+              width="auto"
+              textAlign="center"
+              height={{ base: "100%", md: "150px" }}
+              minW="150px"
+              maxH="150px"
+              bg="white"
+              // borderLeft="4px solid #FF0000"
+              borderRadius="12px"
+              boxShadow="md" // Add a shadow for a card-like appearance
+              p="2" // Adjust padding as needed
+              display="flex"
+              // flexDirection={{ base: "column", md: "column", lg: "column" }}
+              direction={{ base: "column", md: "row", lg: "row" }}
+              justifyContent={{ base: "center", md: "space-around" }}
+              alignItems="center"
+              gap="20px"
+            >
+              {/* <div style={{ flex: 1 }}> */}
+              <img
+                src={
+                  // ""
+                  TotalCumstmer
+                }
+                alt="Total Customers"
+              />
+              <Text
+                fontSize={{ base: "0.7rem", md: "1.2rem" }}
+                fontWeight="bold"
+              >
+                Inactive User
+              </Text>
+              {/* <Heading size="lg" color="black" textAlign="center">
+                  {EmployeeCount}
+                </Heading> */}
+              {/* {users && (
+                  <Heading size="lg" color="black" textAlign="center">
+                    {users.length}
+                    {console.log("users length", users.length)}
+                  </Heading> */}
+
+              {/* </div> */}
+            </Card>
+          </Link>
+        </Flex>
+        <Flex
+          direction={{ md: "column" }}
+          gap="25px"
+          justifyContent="center"
+          marginTop="30px"
+          align="center"
+        >
+          {/* Chnage Link */}
+          <Link
+            to={{
+              pathname: "/f",
+              // state: { users },
+            }}
+          >
+            <Card
+              as="flex"
+              width="auto"
+              textAlign="center"
+              height={{ base: "100%", md: "150px" }}
+              minW="150px"
+              maxH="150px"
+              bg="white"
+              // borderLeft="4px solid #FF0000"
+              borderRadius="12px"
+              boxShadow="md" // Add a shadow for a card-like appearance
+              p="2" // Adjust padding as needed
+              display="flex"
+              // flexDirection={{ base: "column", md: "column", lg: "column" }}
+              direction={{ base: "column", md: "row", lg: "row" }}
+              justifyContent={{ base: "center", md: "space-around" }}
+              alignItems="center"
+              gap="20px"
+            >
+              {/* <div style={{ flex: 1 }}> */}
+              <img
+                src={
+                  // ""
+                  TotalCumstmer
+                }
+                alt="Total Customers"
+              />
+              <Text
+                fontSize={{ base: "0.7rem", md: "1.2rem" }}
+                fontWeight="bold"
+              >
+                Today's Recovery
+              </Text>
+              {/* <Heading size="lg" color="black" textAlign="center">
+                  {EmployeeCount}
+                </Heading> */}
+              {/* {users && (
+                  <Heading size="lg" color="black" textAlign="center">
+                    {users.length}
+                    {console.log("users length", users.length)}
+                  </Heading> */}
+
+              {/* </div> */}
+            </Card>
+          </Link>
+        </Flex>
+      </Flex>
+      <Flex direction={{ base: "column", md: "row" }} gap="25px">
+        <Flex
+          direction={{ md: "column" }}
+          gap="25px"
+          justifyContent="center"
+          marginTop="30px"
+          align="center"
+        >
+          {/* Chnage Link */}
+          <Link
+            to={{
+              pathname: "/f",
+              // state: { users },
+            }}
+          >
+            <Card
+              as="flex"
+              width="auto"
+              textAlign="center"
+              height={{ base: "100%", md: "150px" }}
+              minW="150px"
+              maxH="150px"
+              bg="white"
+              // borderLeft="4px solid #FF0000"
+              borderRadius="12px"
+              boxShadow="md" // Add a shadow for a card-like appearance
+              p="2" // Adjust padding as needed
+              display="flex"
+              // flexDirection={{ base: "column", md: "column", lg: "column" }}
+              direction={{ base: "column", md: "row", lg: "row" }}
+              justifyContent={{ base: "center", md: "space-around" }}
+              alignItems="center"
+              gap="20px"
+            >
+              {/* <div style={{ flex: 1 }}> */}
+              <img
+                src={
+                  // ""
+                  TotalCumstmer
+                }
+                alt="Total Customers"
+              />
+              <Text
+                fontSize={{ base: "0.7rem", md: "1.2rem" }}
+                fontWeight="bold"
+              >
+                Total Recovery
+              </Text>
+              {/* <Heading size="lg" color="black" textAlign="center">
+                  {EmployeeCount}
+                </Heading> */}
+              {/* {users && (
+                  <Heading size="lg" color="black" textAlign="center">
+                    {users.length}
+                    {console.log("users length", users.length)}
+                  </Heading> */}
+
+              {/* </div> */}
+            </Card>
+          </Link>
+        </Flex>
+        <Flex
+          direction={{ md: "column" }}
+          gap="25px"
+          justifyContent="center"
+          marginTop="30px"
+          align="center"
+        >
+          {/* Chnage Link */}
+          <Link
+            to={{
+              pathname: "/f",
+              // state: { users },
+            }}
+          >
+            <Card
+              as="flex"
+              width="auto"
+              textAlign="center"
+              height={{ base: "100%", md: "150px" }}
+              minW="150px"
+              maxH="150px"
+              bg="white"
+              // borderLeft="4px solid #FF0000"
+              borderRadius="12px"
+              boxShadow="md" // Add a shadow for a card-like appearance
+              p="2" // Adjust padding as needed
+              display="flex"
+              // flexDirection={{ base: "column", md: "column", lg: "column" }}
+              direction={{ base: "column", md: "row", lg: "row" }}
+              justifyContent={{ base: "center", md: "space-around" }}
+              alignItems="center"
+              gap="20px"
+            >
+              {/* <div style={{ flex: 1 }}> */}
+              <img
+                src={
+                  // ""
+                  TotalCumstmer
+                }
+                alt="Total Customers"
+              />
+              <Text
+                fontSize={{ base: "0.7rem", md: "1.2rem" }}
+                fontWeight="bold"
+              >
                 Add Employees
               </Text>
               {/* <Heading size="lg" color="black" textAlign="center">
