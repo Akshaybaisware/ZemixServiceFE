56d10a97d5cd5ab756cd4af38fbdebefc442acfd
Kaustubh <kaustubhr2001@gmail.com>
2024-04-05 22:57:42 +0530

    add client api done intigratin

--- src/Components/Registeration/AddClient.jsx
+++ src/Components/Registeration/AddClient.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React from "react";
 import { useForm } from "react-hook-form";
 import { VStack } from "@chakra-ui/layout";
 import { FormControl, FormLabel } from "@chakra-ui/form-control";
@@ -16,65 +16,38 @@ function AddClient() {
 
   const toast = useToast();
 
-  const onSubmit = (data) => {
-    console.log(data);
-  };
-
-  const name = useRef();
-  const address = useRef();
-  const email = useRef();
-  const mobile = useRef();
-  const plan = useRef();
-  const selectPlan = useRef();
-
-  const handlesubmitclient = async () => {
+  const onSubmit = async (data) => {
     try {
-      console.log("called");
-      console.log(
-        name.current.value,
-        address.current.value,
-        email.current.value,
-        mobile.current.value,
-        plan.current.value,
-        selectPlan.current.value,
-        "admafmiandfoin"
-      );
-      const response = await fetch(
-        "http://localhost:5000/api/client/addclient",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            name: name.current.value,
-            address: address.current.value,
-            email: email.current.value,
-            mobile: mobile.current.value,
-            plan: plan.current.value,
-            selectPlan: selectPlan.current.value,
-          }),
-        }
-      );
+      console.log(data);
+      const response = await fetch("http://localhost:5000/api/user/addclient", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
       const responseData = await response.json();
       console.log(responseData);
-      // if (responseData.isAdded) {
-      //   toast({
-      //     title: "Success",
-      //     description: "Client added successfully",
-      //     status: "success",
-      //     duration: 9000,
-      //     isClosable: true,
-      //   });
-      // } else {
-      //   toast({
-      //     title: "Error",
-      //     description: "Client not added",
-      //     status: "error",
-      //     duration: 9000,
-      //     isClosable: true,
-      //   });
-      // }
+      if (responseData.isAdded) {
+        toast({
+          title: "Success",
+          description: "Client added successfully",
+          position: "top",
+          status: "success",
+          duration: 3000,
+
+          isClosable: true,
+        });
+      } else {
+        toast({
+          title: "Error",
+          description: "Client not added",
+          status: "error",
+          duration: 3000,
+          isClosable: true,
+          position: "top",
+        });
+      }
     } catch (error) {
       console.log(error);
     }
@@ -87,44 +60,30 @@ function AddClient() {
         <VStack spacing={4}>
           <FormControl>
             <FormLabel htmlFor="name">Name</FormLabel>
-            <Input ref={name} type="text" id="name" {...register("name")} />
+            <Input type="text" id="name" {...register("name")} />
           </FormControl>
           <FormControl>
             <FormLabel htmlFor="address">Address</FormLabel>
-            <Input
-              ref={address}
-              type="text"
-              id="address"
-              {...register("address")}
-            />
+            <Input type="text" id="address" {...register("address")} />
           </FormControl>
           <FormControl>
             <FormLabel htmlFor="email">Email</FormLabel>
-            <Input ref={email} type="email" id="email" {...register("email")} />
+            <Input type="email" id="email" {...register("email")} />
           </FormControl>
           <FormControl>
             <FormLabel htmlFor="mobile">Mobile No</FormLabel>
-            <Input
-              ref={mobile}
-              type="tel"
-              id="mobile"
-              {...register("mobile")}
-            />
+            <Input type="tel" id="mobile" {...register("mobile")} />
           </FormControl>
           <FormControl>
             <FormLabel htmlFor="plan">Plan</FormLabel>
-            <Select ref={plan} id="plan" {...register("plan")}>
+            <Select id="plan" {...register("plan")}>
               <option value="-">Plan</option>
               <option value="540">540</option>
             </Select>
           </FormControl>
           <FormControl>
             <FormLabel htmlFor="selectPlan">Select Plan</FormLabel>
-            <Select
-              ref={selectPlan}
-              id="selectPlan"
-              {...register("selectPlan")}
-            >
+            <Select id="selectPlan" {...register("selectPlan")}>
               <option value="-">Select Plan</option>
               <option value="1">1</option>
               <option value="2">2</option>
@@ -134,9 +93,7 @@ function AddClient() {
               <option value="6">6</option>
             </Select>
           </FormControl>
-          <Button onClick={handlesubmitclient} type="submit">
-            Submit
-          </Button>
+          <Button type="submit">Submit</Button>
         </VStack>
       </form>
     </>
