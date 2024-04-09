b6571b26c889a0a66576d3a75f450e455db1ad11
Kaustubh <kaustubhr2001@gmail.com>
2024-04-09 15:35:19 +0530

    userlogin done

--- src/Components/Login/UserLogin.jsx
+++ src/Components/Login/UserLogin.jsx
@@ -4,14 +4,58 @@ import logo from "../../assets/logo.png";
 import { NavLink } from "react-router-dom";
 import { Box, Button, Center, Image, Input } from "@chakra-ui/react";
 import axios from "axios";
+import { useToast } from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
 
 function UserLogin() {
-  
+  const username = useRef();
+  const password = useRef();
+  const toast = useToast();
+  const navigate = useNavigate();
+
+  const handleloginuser = async () => {
+    try {
+      console.log("asd");
+      const user = {
+        email: username.current.value,
+        password: password.current.value,
+      };
+      const response = await axios.post(
+        "http://localhost:5000/api/user/login",
+        user
+      );
+      console.log(response.data);
+
+      if (response.data.message == "Login success..") {
+        localStorage.setItem("token", response.data.token);
+        localStorage.setItem("role", response.data.role);
+        localStorage.setItem("email", response.data.email);
+        toast({
+          title: "Login",
+          description: "Login Successfully",
+          status: "success",
+          duration: 3000,
+          position: "top",
+          isClosable: true,
+        });
+        navigate("/");
+      }
+    } catch (error) {
+      toast({
+        title: "Error",
+        description: "Error in Login",
+        status: "error",
+        duration: 3000,
+        position: "top",
+        isClosable: true,
+      });
+    }
+  };
+
   return (
     <>
       <Box
         bg={"lightgray"}
-        
         display="flex"
         flexDirection="column"
         alignItems="center"
@@ -20,9 +64,11 @@ function UserLogin() {
       >
         <Box width={["300px", "500px"]} padding={"2rem"} bg={"white"}>
           <Center
-          textAlign={"center"}
-          color={"purple"}
-          fontSize={"1.2rem"} fontWeight={"700"}>
+            textAlign={"center"}
+            color={"purple"}
+            fontSize={"1.2rem"}
+            fontWeight={"700"}
+          >
             Welcome back ! Log in to your account to Complete Your work
           </Center>
 
@@ -32,41 +78,33 @@ function UserLogin() {
             flexDirection="column"
             alignItems="center"
             textAlign={"center"}
-            // Set a maximum width for responsiveness
-            // Take up 90% of the parent container's width
-            mt="20px" // Add margin at the top
+            mt="20px"
           >
             <Box width="100%" mb="20px">
-              {" "}
-              {/* Adjust width and add margin-bottom */}
-              <Input  placeholder="UserName" />
+              <Input ref={username} placeholder="UserName" />
             </Box>
             <Box width="100%" mb="20px">
-              {" "}
-              {/* Adjust width and add margin-bottom */}
-              <Input  placeholder="Password" />
+              <Input ref={password} placeholder="Password" />
             </Box>
             <Button
               height="3rem"
-              width="40%" // Take up 100% width
+              width="40%"
               borderRadius="16px"
               color="white"
               background="#FF00FF"
               fontSize={["0.9rem", "1.3rem"]}
               fontWeight={700}
               fontFamily='"Poppins", sans-serif'
-              mb="20px" // Add margin-bottom
+              mb="20px"
               _hover={{ background: "FloralWhite", color: "black" }}
-            
+              onClick={handleloginuser}
             >
               Log In
             </Button>
+
             <NavLink
-            
               style={{ textDecoration: "none", width: "100%" }}
-            >
-            
-            </NavLink>
+            ></NavLink>
           </Box>
         </Box>
       </Box>
