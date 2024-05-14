396936ae1604ac33f889921099689625666f091e
Kaustubh Raut <87487138+kaustubhraut2001@users.noreply.github.com>
2024-05-14 11:33:54 +0530

    Update Login.jsx

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -162,7 +162,7 @@ function Login() {
 
         toast({
           title: "Login Success",
-          description: "Welcome to Zemix Service",
+          description: "Welcome to Croptonservice Service",
           status: "success",
           duration: 3000,
           position: "top",
